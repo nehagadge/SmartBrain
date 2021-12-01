9fcf57d8468e91bfe648c4f2747215bbaa758a72
Neha Gadge <gadge.neha@gmail.com>
2021-12-01 11:01:05 -0800

    cleanup and destructuring

--- src/App.js
+++ src/App.js
@@ -54,7 +54,6 @@ class App extends Component {
   }
 
   displayFaceBox = (box) => {
-    console.log(box)
     this.setState ({box:box})
   }
 
@@ -81,20 +80,21 @@ class App extends Component {
 
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App" >
         <Particles className='particles'
               params={particlesOptions}
         />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
         {
-          this.state.route === 'home' ?
+          route === 'home' ?
             <div>
             <Logo />
             <Rank />
             < ImageLinkForm onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit} />
-            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
             :  (
               this.state.route === 'signin'
